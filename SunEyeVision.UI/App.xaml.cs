95b28024d0e5edd9b09947a84b18243fac4ca756
houzj <757935369@qq.com>
2026-01-27 11:16:16 +0800

    Revert "重构了连线架构，将路径计算逻辑从 UI 层分离到服务层，"
    This reverts commit bdf5c86f9450a5b11489baede3a00bb114cf170a.

--- SunEyeVision.UI/App.xaml.cs
+++ SunEyeVision.UI/App.xaml.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Text;
 using System.Windows;
 
 namespace SunEyeVision.UI;
@@ -13,18 +11,9 @@ public partial class App : Application
     {
         base.OnStartup(e);
 
-        try
-        {
-            Console.OutputEncoding = Encoding.UTF8;
-            // 显示主窗口
-            var mainWindow = new MainWindow();
-            mainWindow.Show();
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show($"应用程序启动失败: {ex.Message}\n\n堆栈跟踪: {ex.StackTrace}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
-            Environment.Exit(1);
-        }
+        // 显示主窗口
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
     }
 }
 
